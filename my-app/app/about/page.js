aa146b44ea578010b315582ce9e709318741d177
sethyadav <sethyadav169@gmail.com>
2026-09-22 14:12:42 +0530

    Update my-app website

---
+++ my-app/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>Problem solved by Next.js</h1>
+      <ul>
+        <li>Full stack </li>
+        <li>File based Routing</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>optimize rendring</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
